6785e0dd5d93692e71d4cac08cdf138bb86b517b
ten.datngo <quocdat32461997@yahoo.com>
2022-03-19 17:13:53 -0500

    Create configs.py

---
+++ configs.py
@@ -0,0 +1,39 @@
+import os
+import json
+import torch
+
+
+class Configs(object):
+    def __init__(self, path=None, args=None):
+        self.args = args
+        if path is not None:
+            assert os.path.exists(path) and path.endswith('json')
+            with open(path, 'r') as file:
+                self.from_json(json.load(file))
+        elif args is not None:
+            assert isinstance(args, dict) is True
+            self.from_json(args)
+        else:
+            raise Exception("Either path or args must be a valid value")
+
+    def to_cuda(self):
+        if torch.cuda.is_available():
+            device = self.device_name if self.device_name is not None else 'cuda:0'
+        else:
+            device = 'cpu'
+
+        self.device = torch.device(device)
+
+    def from_json(self, configs):
+        self.__dict__.update(configs)
+        self.args = {k: v for k, v in self.__dict__.items()}  # save args
+
+        # create saved_dir
+        if not os.path.exists(self.save_dir):
+            os.mkdir(self.save_dir)
+
+    def to_json(self, path):
+        assert isinstance(path, str) and path.endswith('.json')
+
+        with open(path, 'w') as file:
+            json.dump(self.args, file)
