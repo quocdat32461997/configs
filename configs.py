f06ae9a062983f8c0eb79b64a471b9926122dca4
quocdat32461997 <quocdat32461997@yahoo.com>
2023-01-27 13:34:07 -0600

    Updates to align with pytorch-lightning

--- configs.py
+++ configs.py
@@ -7,8 +7,8 @@ class Configs(object):
     def __init__(self, path=None, args=None):
         self.args = args
         if path is not None:
-            assert os.path.exists(path) and path.endswith('json')
-            with open(path, 'r') as file:
+            assert os.path.exists(path) and path.endswith("json")
+            with open(path, "r") as file:
                 self.from_json(json.load(file))
         elif args is not None:
             assert isinstance(args, dict) is True
@@ -16,8 +16,9 @@ class Configs(object):
         else:
             raise Exception("Either path or args must be a valid value")
 
-        if self.device is None:
-            self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        if not self.devices:
+            self.devices = "auto"
+        self.accelerator = "cuda" if torch.cuda.is_available() else "cpu"
 
     def from_json(self, configs):
         self.__dict__.update(configs)
@@ -28,7 +29,7 @@ class Configs(object):
             os.mkdir(self.save_dir)
 
     def to_json(self, path):
-        assert isinstance(path, str) and path.endswith('.json')
+        assert isinstance(path, str) and path.endswith(".json")
 
-        with open(path, 'w') as file:
+        with open(path, "w") as file:
             json.dump(self.args, file)
